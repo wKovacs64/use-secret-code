f2bad03af2a06f6f76678422c9ec80894b0f0018
Justin Hall <justin.r.hall@gmail.com>
2020-06-19 09:25:46 -0600

    chore: update prettier targets (#14)
    * chore: update prettier targets
    
    * Update lint-staged.config.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{js,ts,tsx}': ['tsdx lint --fix'],
-  '*.{html,json,md,yml,yaml}': ['prettier --write'],
+  '*.{js,jsx,ts,tsx}': ['tsdx lint --fix'],
+  '*.{html,css,json,md,mdx,yml,yaml}': ['prettier --write'],
 };
