7765beb0609d6f31628882dab39cdc1a44aa072d
Justin Hall <justin.r.hall@gmail.com>
2021-03-02 17:04:55 -0700

    build(example): replace parcel with vite (#45)

---
+++ example/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import legacy from '@vitejs/plugin-legacy';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh(), legacy({ targets: ['defaults', 'IE 11'] })],
+});
