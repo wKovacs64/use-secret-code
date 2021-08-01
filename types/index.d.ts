0391256f823c9da2bd0745e2a12b1f1744f7f314
Justin Hall <justin.r.hall@gmail.com>
2021-08-01 08:18:44 -0600

    refactor: replace XState typestate with a model

--- types/index.d.ts
+++
@@ -1,9 +0,0 @@
-export interface KeyTrackingContext {
-  typedKeys: Array<string>;
-}
-
-export type KeyTrackingEvent = KeyboardEvent;
-
-export type KeyTrackingState =
-  | { value: 'disabled'; context: KeyTrackingContext }
-  | { value: 'enabled'; context: KeyTrackingContext };
