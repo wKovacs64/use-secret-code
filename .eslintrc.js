86c9f38365452d9fd8f90d5aa2ccd6d8891c8ac4
Justin Hall <justin.r.hall@gmail.com>
2021-11-20 10:17:32 -0700

    chore: use tsdx's lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   extends: [
-    'plugin:wkovacs64/react',
-    'plugin:wkovacs64/jest',
-    'plugin:wkovacs64/jest-dom',
-    'plugin:wkovacs64/testing-library',
-    'plugin:wkovacs64/typescript',
-    'prettier',
+    'react-app',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
