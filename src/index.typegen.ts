82f2257f3e40bef4e91f7dcd89aac1476e1323bf
Justin Hall <justin.r.hall@gmail.com>
2023-04-30 14:49:44 -0600

    chore: update XState typegen

--- src/index.typegen.ts
+++ src/index.typegen.ts
@@ -1,45 +1,38 @@
-// This file was automatically generated. Edits will be overwritten
 
-export interface Typegen0 {
-  '@@xstate/typegen': true;
-  internalEvents: {
-    '': { type: '' };
-    'xstate.after(doneTyping)#cheatCodeMachine.disabled.recording': {
-      type: 'xstate.after(doneTyping)#cheatCodeMachine.disabled.recording';
-    };
-    'xstate.after(doneTyping)#cheatCodeMachine.enabled.recording': {
-      type: 'xstate.after(doneTyping)#cheatCodeMachine.enabled.recording';
-    };
-    'xstate.init': { type: 'xstate.init' };
-  };
-  invokeSrcNameMap: {};
-  missingImplementations: {
-    actions: never;
-    services: never;
-    guards: never;
-    delays: never;
-  };
-  eventsCausingActions: {
-    record: 'keydown';
-    resetTypedKeys:
-      | ''
-      | 'xstate.after(doneTyping)#cheatCodeMachine.disabled.recording'
-      | 'xstate.after(doneTyping)#cheatCodeMachine.enabled.recording';
-  };
-  eventsCausingServices: {};
-  eventsCausingGuards: {
-    cheatCodeEntered: '';
-  };
-  eventsCausingDelays: {
-    doneTyping: 'keydown';
-  };
-  matchesStates:
-    | 'disabled'
-    | 'disabled.idle'
-    | 'disabled.recording'
-    | 'enabled'
-    | 'enabled.idle'
-    | 'enabled.recording'
-    | { disabled?: 'idle' | 'recording'; enabled?: 'idle' | 'recording' };
-  tags: never;
-}
+  // This file was automatically generated. Edits will be overwritten
+
+  export interface Typegen0 {
+        '@@xstate/typegen': true;
+        internalEvents: {
+          "": { type: "" };
+"xstate.after(doneTyping)#cheatCodeMachine.disabled.recording": { type: "xstate.after(doneTyping)#cheatCodeMachine.disabled.recording" };
+"xstate.after(doneTyping)#cheatCodeMachine.enabled.recording": { type: "xstate.after(doneTyping)#cheatCodeMachine.enabled.recording" };
+"xstate.init": { type: "xstate.init" };
+        };
+        invokeSrcNameMap: {
+          
+        };
+        missingImplementations: {
+          actions: never;
+          delays: never;
+          guards: never;
+          services: never;
+        };
+        eventsCausingActions: {
+          "record": "keydown";
+"resetTypedKeys": "" | "xstate.after(doneTyping)#cheatCodeMachine.disabled.recording" | "xstate.after(doneTyping)#cheatCodeMachine.enabled.recording";
+        };
+        eventsCausingDelays: {
+          "doneTyping": "keydown";
+        };
+        eventsCausingGuards: {
+          "cheatCodeEntered": "";
+        };
+        eventsCausingServices: {
+          
+        };
+        matchesStates: "disabled" | "disabled.idle" | "disabled.recording" | "enabled" | "enabled.idle" | "enabled.recording" | { "disabled"?: "idle" | "recording";
+"enabled"?: "idle" | "recording"; };
+        tags: never;
+      }
+  
