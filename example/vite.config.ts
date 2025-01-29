52d3a315e9718db7097a161083eb5ebcbb61def0
Justin Hall <justin.r.hall@gmail.com>
2025-01-28 21:25:49 -0700

    Remove legacy browser support from example

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    // required when targeting legacy browsers
-    minify: 'terser',
-  },
-  plugins: [react(), legacy({ targets: ['defaults'] })],
+  plugins: [react()],
 });
