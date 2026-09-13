ba62af1573834f8a27cd481546fdc79d948d6f2d
Justin Hall <justin.r.hall@gmail.com>
2026-09-13 12:01:54 -0600

    test: use Vitest-specific jest-dom matchers

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { useCheatCode } from "../src";
--- test/setup.ts
+++ test/setup.ts
@@ -1,2 +1,4 @@
+import "@testing-library/jest-dom/vitest";
+
 // @ts-expect-error
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
